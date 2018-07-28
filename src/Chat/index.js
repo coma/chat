89d6ebb14b498165460fd52ad912acc16955798d
coma <eduardo@comakai.com>
2018-07-28 22:12:30 +0200

    chat style

--- src/Chat/index.js
+++ src/Chat/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../store';
+import style from './style.module.css';
 
 export default () => (
   <Provider store={store}>
-    <div>hola</div>
+    <div className={style.chat}>hola</div>
   </Provider>
 );
